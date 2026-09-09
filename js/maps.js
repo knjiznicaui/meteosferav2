0c63b387c4b36bf220f5c16aeb81b3e61a1b3d03
knjiznicaui <autointerviews@gmail.com>
2026-09-09 20:18:43 +0200

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -26,14 +26,11 @@ const Maps = {
     snow:    'https://tile.openweathermap.org/map/snow/{z}/{x}/{y}.png'
   },
 
-  // Dark OSM tiles
-  DARK_TILES: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-  LIGHT_TILES: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-  OSM_TILES:   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  // OpenStreetMap tiles
+  OSM_TILES: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 
   getTileLayer() {
-    const theme = Settings.get('theme') || 'dark';
-    return theme === 'light' ? this.LIGHT_TILES : this.DARK_TILES;
+    return this.OSM_TILES;
   },
 
   // ---- INIT MINI MAP ----
@@ -48,8 +45,8 @@ const Maps = {
     });
 
     L.tileLayer(this.getTileLayer(), {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OSM</a> © <a href="https://carto.com/">CARTO</a>',
-      subdomains: 'abcd', maxZoom: 19
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abc', maxZoom: 19
     }).addTo(this.miniMap);
 
     // Weather layer (clouds free)
@@ -105,7 +102,7 @@ const Maps = {
     });
 
     this._fullBaseLayer = L.tileLayer(this.getTileLayer(), {
-      attribution: '© OSM © CARTO', subdomains: 'abcd', maxZoom: 19
+      attribution: '© OpenStreetMap contributors', subdomains: 'abc', maxZoom: 19
     }).addTo(this.fullMap);
 
     // Add weather layer
@@ -212,7 +209,7 @@ const Maps = {
     if (!el) return;
 
     this.radarMap = L.map('radarMap', { center: [lat, lon], zoom: 6 });
-    L.tileLayer(this.getTileLayer(), { attribution: '© OSM © CARTO', subdomains: 'abcd', maxZoom: 19 }).addTo(this.radarMap);
+    L.tileLayer(this.getTileLayer(), { attribution: '© OpenStreetMap contributors', subdomains: 'abc', maxZoom: 19 }).addTo(this.radarMap);
     L.circleMarker([lat, lon], { radius: 7, fillColor: '#4FC3F7', color: '#00E5FF', weight: 2, fillOpacity: 0.9 }).addTo(this.radarMap);
 
     await this._reloadRadarFrames();
